c49d2966129d5f2b7ae35cc214cbedf09078bf8f
Mahmudul Hasan <mahamudul2018@gmail.com>
2025-05-01 22:42:56 +0600

    create brands section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/home/Banner";
+import Brands from "../components/home/Brands";
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Brands />
         </div>
     );
 };
